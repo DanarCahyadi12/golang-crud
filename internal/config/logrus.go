c83bd4dcd7cc828e1e9a43e145093a3d6ba77f20
DanarCahyadi12 <danarcahyadi21@gmail.com>
2024-04-03 14:43:36 +0800

    chore: add colored text on logrus config

--- internal/config/logrus.go
+++ internal/config/logrus.go
@@ -4,5 +4,9 @@ import "github.com/sirupsen/logrus"
 
 func NewLogrus() *logrus.Logger {
 	logger := logrus.New()
+	logger.SetFormatter(&logrus.TextFormatter{
+		ForceColors:   true,
+		FullTimestamp: true,
+	})
 	return logger
 }
