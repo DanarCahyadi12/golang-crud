b63c3bdc9bd30b047bef48d8a53f655ad164d13b
DanarCahyadi12 <danarcahyadi21@gmail.com>
2024-04-08 19:42:51 +0800

    fix: change data type price from string to integer on product entity

--- internal/entity/product_entity.go
+++ internal/entity/product_entity.go
@@ -3,7 +3,7 @@ package entity
 type Product struct {
 	Id     string `gorm:"column:id;primaryKey"`
 	Name   string `gorm:"column:name"`
-	Price  string `gorm:"column:price"`
+	Price  int    `gorm:"column:price"`
 	Stock  int    `gorm:"column:stock"`
 	UserId string `gorm:"column:user_id;"`
 	User   User   `gorm:"foreignKey:user_id;references:id"`
